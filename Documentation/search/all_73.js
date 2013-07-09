5cc11e4ec99364493c5862c7c519fb74b01c1351
Kartik Kumar <me@kartikkumar.com>
2013-07-09 10:03:17 +0200

    Removed Database sub-directory (substituted in my code with SQLiteCpp library). Added COPYING license statement. Updated license reference in file headers. Fixed Doxygen undocumented elements.

--- Documentation/search/all_73.js
+++ Documentation/search/all_73.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['setcurrenttimeandstate',['setCurrentTimeAndState',['../classassist_1_1astrodynamics_1_1_body.html#a6a59c41a2f9036455d36985891065919',1,'assist::astrodynamics::Body']]],
-  ['sqlite3databaseconnector',['Sqlite3DatabaseConnector',['../classassist_1_1database_1_1_sqlite3_database_connector.html#a43595391ee044ed9e0a2ed3d339efa58',1,'assist::database::Sqlite3DatabaseConnector']]],
-  ['sqlite3databaseconnector',['Sqlite3DatabaseConnector',['../classassist_1_1database_1_1_sqlite3_database_connector.html',1,'assist::database']]]
+  ['setcurrenttimeandstate',['setCurrentTimeAndState',['../classassist_1_1astrodynamics_1_1_body.html#a6a59c41a2f9036455d36985891065919',1,'assist::astrodynamics::Body']]]
 ];
--- Documentation/search/functions_63.js
+++ Documentation/search/functions_63.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['closedatabase',['closeDatabase',['../classassist_1_1database_1_1_sqlite3_database_connector.html#ad7ceba3b45e213d8214ae84b65b5dfe6',1,'assist::database::Sqlite3DatabaseConnector']]],
   ['comparedoublekeydoublevaluemaprelativedistances',['CompareDoubleKeyDoubleValueMapRelativeDistances',['../classassist_1_1basics_1_1_compare_double_key_double_value_map_relative_distances.html#aaa6416cb92273e568771992bf056cf6d',1,'assist::basics::CompareDoubleKeyDoubleValueMapRelativeDistances']]]
 ];
